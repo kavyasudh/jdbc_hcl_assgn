b8229b48faaa93d777e07f86ce41dc4d17ccd4fd
kavyasudh <63489483+kavyasudh@users.noreply.github.com>
2020-11-07 17:46:06 +0530

    Add files via upload

---
+++ CustomerController.java
@@ -0,0 +1,67 @@
+package com.customer.web.controller;
+
+import java.sql.Connection;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import com.customer.model.dao.ConnectionFactory;
+import com.customer.model.dao.Customer;
+import com.customer.model.dao.CustomerDao;
+import com.customer.model.dao.CustomerDaoImpl;
+
+
+public class CustomerController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Connection connection = ConnectionFactory.getConnection();
+		if (connection != null)
+			System.out.println("sucess");
+
+		CustomerDao dao = new CustomerDaoImpl();
+
+// add customer
+		
+		/*
+		 * Customer customer=new Customer(5,"sathish", "8464964782", "sat@gmail.com",
+		 * new Date(),"sklm",7550000); dao.addCustomer(customer);
+		 * System.out.println("---------------");
+		 */
+		 
+// Delete the customer
+
+		/*
+		 * dao.delCustomer(5);
+		 */
+		
+		  
+// update the customer
+		/*
+		 * dao.updateCustomer(4,200000); System.out.println("updated.....");
+		 * 
+		 * 
+		 */
+		
+		  
+// print all the customers
+
+		/*
+		 * List<Customer>customers=dao.getAllCustomer(); customers.forEach(c->
+		 * System.out.println(c));
+		 * 
+		 * 
+		 */
+
+// get customer by id
+
+		System.out.println(dao.getCustomerById(3));
+
+// get selected customer
+
+		List<Customer> customers = dao.getSelectedCustomer(500000);
+		customers.forEach(c -> System.out.println(c));
+
+	}
+
+}
---
+++ CustomerDaoImpl.java
@@ -0,0 +1,158 @@
+package com.customer.model.dao;
+
+import java.sql.Connection;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Optional;
+import java.util.ArrayList;
+
+
+
+
+
+public class CustomerDaoImpl implements CustomerDao {
+
+	
+	private Connection connection;
+
+	public CustomerDaoImpl() {
+		connection = ConnectionFactory.getConnection();
+	}
+
+	@Override
+	public void addCustomer(Customer customer){
+		// TODO Auto-generated method stub
+		try {
+			PreparedStatement pstmt = connection
+					.prepareStatement
+					("insert into customer_details(id,name,phone,email,dob,address,purchase_capacity) values (?,?,?,?,?,?,?)");
+			pstmt.setInt(1,customer.getId());
+			pstmt.setString(2,customer.getName());
+			pstmt.setString(3, customer.getPhone());
+			pstmt.setString(4, customer.getEmail());
+			pstmt.setDate(5, new Date(customer.getDob().getTime()));
+			pstmt.setString(6, customer.getAddress());
+			pstmt.setDouble(7, customer.getPurchase_capacity());
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+		   e.printStackTrace();
+		}
+
+		
+	}
+
+	@Override
+	public void delCustomer(int id) {
+		// TODO Auto-generated method stub
+		Optional<Customer> customer=getCustomerById(id);
+		try {
+			PreparedStatement pstmt = connection
+					.prepareStatement("delete from customer_details where id=?");
+			pstmt.setInt(1,id);
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void updateCustomer(int id, double purchase_capacity) {
+		// TODO Auto-generated method stub
+		Optional<Customer> customer=getCustomerById(id);
+		try {
+			PreparedStatement pstmt = connection
+					.prepareStatement("update customer_details set purchase_capacity=? where id=?");
+			pstmt.setDouble(1, purchase_capacity);
+			pstmt.setInt(2,id);
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public List<Customer> getAllCustomer() {
+		// TODO Auto-generated method stub
+		List<Customer> customers = new ArrayList<>();
+		Customer customer = null;
+		try {
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from customer_details");
+
+			while (rs.next()) {
+				
+
+				customer = new Customer(rs.getInt("id"), rs.getString("name"), rs.getString("phone"), rs.getString("email"),
+						rs.getDate("dob"),rs.getString("address"), rs.getDouble("purchase_capacity"));
+
+				customers.add(customer);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return customers;
+		
+	}
+
+	@Override
+	public Optional<Customer> getCustomerById(int id) {
+		// TODO Auto-generated method stub
+		Customer customer = null;
+		try {
+			PreparedStatement pstmt = connection.prepareStatement("select * from customer_details where id=?");
+			pstmt.setInt(1, id);
+
+			ResultSet rs = pstmt.executeQuery();
+			if (rs.next()) {
+				customer = new Customer(rs.getInt("id"), rs.getString("name"), rs.getString("phone"), rs.getString("email"),
+						rs.getDate("dob"),rs.getString("address"), rs.getDouble("purchase_capacity"));
+
+				
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return Optional.ofNullable(customer);
+	}
+
+	
+
+	@Override
+	public List<Customer> getSelectedCustomer(double purchase_capacity) {
+		// TODO Auto-generated method stub
+		        List<Customer> customers = new ArrayList<>();
+				Customer customer = null;
+				try {
+					PreparedStatement pstmt = connection.prepareStatement("select * from customer_details where purchase_capacity>?");
+					pstmt.setDouble(1, purchase_capacity);
+
+					ResultSet rs = pstmt.executeQuery();
+					while (rs.next()) {
+						customer = new Customer(rs.getInt("id"), rs.getString("name"), rs.getString("phone"), rs.getString("email"),
+								rs.getDate("dob"),rs.getString("address"), rs.getDouble("purchase_capacity"));
+						customers.add(customer);
+						
+					}
+
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				return customers;
+			}
+
+			
+	}
+
+
